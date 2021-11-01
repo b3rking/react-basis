86668c4ca855bf225234b28a67fe87c33eb7b982
b3rking <berkingdaquan@gmail.com>
2021-11-01 15:09:36 +0200

    showing the date and day

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
+
 
 function ExpenseItem(props) {
 
-    // const expenseDate = new Date(2021, 2, 28);
-    // const expenseTitle = 'car insurance';
-    // const expensePrice = 236.21;
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate month={month} year={year} day={day}></ExpenseDate>
+            
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
             </div>
