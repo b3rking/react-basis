1e8fe0cb587e8b370067b75c5469495b6979d122
b3rking <berkingdaquan@gmail.com>
2021-11-02 10:42:13 +0200

    created another component

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,11 +1,16 @@
 function ExpenseDate(props) {
+    
+    const month = props.date.toLocaleString('en-US', {month: 'long'})
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
+    
     return (
         <div>
-            <div>{props.month}</div>
-            <div>{props.day}</div>
-            <div>{props.year}</div>
+            <div>{month}</div>
+            <div>{day}</div>
+            <div>{year}</div>
         </div>
     );
 }
 
-export default ExpenseDate()
+export default ExpenseDate;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,13 +4,9 @@ import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-    const month = props.date.toLocaleString('en-US', {month: 'long'})
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('en-US', {day: '2-digit'})
-
     return (
         <div className="expense-item">
-            <ExpenseDate month={month} year={year} day={day}></ExpenseDate>
+            <ExpenseDate date={props.date}/>
             
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
